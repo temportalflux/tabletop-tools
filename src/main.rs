687fff3b79b8a9a1bd4538d4fbc6df23ffa25bec
temportalflux <dustin.yost.t@gmail.com>
2024-11-10 12:50:08 -0500

    Update tracing logger to suppress yew

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,16 @@ use yew::prelude::*;
 fn main() {
 	logging::wasm::init(logging::wasm::Config::default().prefer_target());
 	console_error_panic_hook::set_once();
-	tracing_wasm::set_as_global_default();
+
+	let _ = tracing::subscriber::set_global_default({
+		use tracing_subscriber::layer::SubscriberExt;
+		let layer = tracing_subscriber::Registry::default();
+		let layer = layer.with(tracing_wasm::WASMLayer::new(tracing_wasm::WASMLayerConfig::default()));
+		let layer = layer.with(tracing_subscriber::filter::filter_fn(|metadata| {
+			!metadata.target().starts_with("yew")
+		}));
+		layer
+	});
 	yew::Renderer::<App>::new().render();
 }
 #[cfg(target_family = "windows")]
